7875fbbdf655e20690a73e02a40136da8c6fe9d0
Avery Lamp <averylamp@gmail.com>
2017-08-16 05:30:34 -0400

    Initial commit

---
+++ HackerNews.py
@@ -0,0 +1,207 @@
+import requests
+import datetime
+
+# --------------- Helpers that build all of the responses ----------------------
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+	return {
+		'outputSpeech': {
+			'type': 'PlainText',
+			'text': output
+		},
+		'card': {
+			'type': 'Simple',
+			'title': title,
+			'content': output
+		},
+		'reprompt': {
+			'outputSpeech': {
+				'type': 'PlainText',
+				'text': reprompt_text
+			}
+		},
+		'shouldEndSession': should_end_session
+	}
+
+
+def build_response(session_attributes, speechlet_response):
+	return {
+		'version': '1.0',
+		'sessionAttributes': session_attributes,
+		'response': speechlet_response
+	}
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_welcome_response():
+	""" If we wanted to initialize the session to have some attributes we could
+	add those here
+	"""
+
+	session_attributes = {}
+	card_title = "Welcome"
+	speech_output = "Welcome to Top Hacker News.  Just tell me how many items you want to hear."
+	# If the user either does not reply to the welcome message or says something
+	# that is not understood, they will be prompted again with this text.
+	reprompt_text = "Say something like 'Give me the top 5'"
+	should_end_session = False
+	return build_response(session_attributes, build_speechlet_response(
+		card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+	card_title = "Session Ended"
+	speech_output = "Thank you for using Top Hacker News. " \
+					"Have a nice day "
+	# Setting this to true ends the session and exits the skill.
+	should_end_session = True
+	return build_response({}, build_speechlet_response(
+		card_title, speech_output, None, should_end_session))
+
+
+# --------------- Intents ------------------
+
+
+
+def handleLookupIntent(intent, old_session):
+	output = ""
+	should_end_session = True
+	session = {}
+	if 'TopNumber' in intent['slots']:
+		print(intent['slots']['TopNumber'])
+		phrase = intent['slots']['TopNumber']
+		fullNumber = 5
+		if 'value' in phrase and phrase.get("value","") != "" and phrase.get("value","")  is not None :
+			fullNumber = int(phrase['value'])
+			print("Full Phrase - {}".format(fullNumber))
+		
+		if fullNumber > 30:
+			fullNumber = 30
+		
+		lookup_results = lookupItems(fullNumber)
+		output = lookup_results
+	return build_response(session, build_speechlet_response("Hacker News Top {}".format(fullNumber), output, output, should_end_session))
+
+
+def lookupItem(item):
+	headers = {"X-Mashape-Key":"QnME8qXj33mshqT4yltM7QQk1Kfjp1vX7zJjsnoN87jXS0bYCf","Accept":"application/json"}
+	r = requests.get("https://community-hacker-news-v1.p.mashape.com/item/{}.json".format(item), headers=headers)
+	results = r.json()
+	print(results)
+	itemTitle = results.get("title", "")
+	return itemTitle
+
+def filterAsciiText(text):
+	return ''.join([i if ord(i) < 128 else ' ' for i in text])
+
+def getListString(listName, function = None):
+	output = ""
+	for i in range(len(listName)):
+		if i == len(listName) - 1:
+			if function is not None:
+				output += " and {}.".format(function(listName[i]))
+			else:
+				output += " and {}.".format(listName[i])
+		else:
+			if function is not None:
+				output += "{}, ".format(function(listName[i]))
+			else:
+				output += "{}, ".format(listName[i])
+	return output
+
+def lookupItems(number):
+	headers = {"X-Mashape-Key":"QnME8qXj33mshqT4yltM7QQk1Kfjp1vX7zJjsnoN87jXS0bYCf","Accept":"application/json"}
+	r = requests.get("https://community-hacker-news-v1.p.mashape.com/topstories.json", headers=headers)
+	result = r.json()
+
+	resultArr = []
+	for count in range(int(number)):
+		item = filterAsciiText(lookupItem(result[count]))
+		print(item)
+		resultArr.append(item)
+		print("Found Item {} / {}, - {}".format(count + 1, number, item))
+	return "Here is the top {} items.  {}".format(number, getListString(resultArr))
+
+print(lookupItems(4))
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+	""" Called when the session starts """
+
+	print("on_session_started requestId=" + session_started_request['requestId']
+		  + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+	""" Called when the user launches the skill without specifying what they
+	want
+	"""
+
+	print("on_launch requestId=" + launch_request['requestId'] +
+		  ", sessionId=" + session['sessionId'])
+	# Dispatch to your skill's launch
+	return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+	""" Called when the user specifies an intent for this skill """
+
+	print("on_intent requestId=" + intent_request['requestId'] +
+		  ", sessionId=" + session['sessionId'])
+
+	intent = intent_request['intent']
+	intent_name = intent_request['intent']['name']
+	r = requests.get("http://google.com")
+	
+	# Dispatch to your skill's intent handlers
+	if intent_name == "GetTop":
+		print("Get information intent detected")
+		return handleLookupIntent(intent, session)
+	elif intent_name == "AMAZON.HelpIntent":
+		return get_welcome_response()
+	elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+		return handle_session_end_request()
+	else:
+		raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+	""" Called when the user ends the session. 
+
+	Is not called when the skill returns should_end_session=true
+	"""
+	print("on_session_ended requestId=" + session_ended_request['requestId'] +
+		  ", sessionId=" + session['sessionId'])
+	# add cleanup logic here
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+	""" Route the incoming request based on type (LaunchRequest, IntentRequest,
+	etc.) The JSON body of the request is provided in the event parameter.
+	"""
+	print("event.session.application.applicationId=" +
+		  event['session']['application']['applicationId'])
+
+	"""
+	Uncomment this if statement and populate with your skill's application ID to
+	prevent someone else from configuring a skill that sends requests to this
+	function.
+	"""
+	# if (event['session']['application']['applicationId'] !=
+	#         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+	#     raise ValueError("Invalid Application ID")
+
+	if event['session']['new']:
+		on_session_started({'requestId': event['request']['requestId']},
+						   event['session'])
+
+	if event['request']['type'] == "LaunchRequest":
+		return on_launch(event['request'], event['session'])
+	elif event['request']['type'] == "IntentRequest":
+		return on_intent(event['request'], event['session'])
+	elif event['request']['type'] == "SessionEndedRequest":
+		return on_session_ended(event['request'], event['session'])
+
+
---
+++ app.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+import HackerNews
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def loadingPage():
+	return "Hello loading page"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	req = request.get_json(silent=True, force=True)
+
+	print("Request Recieved:")
+	# print(json.dumps(req, indent=4))
+
+	res = processRequest(req)
+	print("Response: ")
+	print(res["speech"])
+	res = json.dumps(res, indent=4)
+	
+	r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
+
+
+def processRequest(req):
+	if req.get("result").get("action") == "LookUpClass":
+		print("Class Lookup Detected")
+		return MITClass.lookupClass(req)
+	if req.get("result").get("action") == "LookUpClass.LookUpClassInformation":
+		print("Class Info Lookup Detected")
+		return MITClass.lookupClass(req)
+	if req.get("result").get("action") == "LookUpPerson":
+		print("People Lookup Detected")
+		return MITPeople.lookupPerson(req)
+	if req.get("result").get("action") == "LookUpPerson.LookUpInformation":
+		print("People Lookup Detected")
+		return MITPeople.lookupInformation(req)
+	if req.get("result").get("action") == "LookUpPerson.LookUpConfirmation":
+		print("People Confirmation Detected")
+		return MITPeople.confirmPerson(req)
+	if req.get("result").get("action") == "EndIntent":
+		return endIntent()
+	if req.get("result").get("action") == "LookUpDining":
+		print("Dining Lookup Detected")
+		return MITDining.handle_dining_intent(req)
+	
+	return {
+		"speech": "Unable to proccess the request.  Try again later please.",
+		"displayText": "Unable to proccess the request.  Try again later please.",
+		# "data": data,
+		"contextOut": [],
+		"source": "webhook"
+	}
+
+def endIntent():
+	data =  {"google":{
+	  "expect_user_response":False,
+	  "rich_response":{
+		 "items":[
+			{
+			   "simpleResponse":{
+				  "textToSpeech":"Thank you for using MIT Information.  Keep us in mind when you need more of your on campus information.",
+				  "displayText":"Thank you for using MIT Information.  Keep us in mind when you need more of your on campus information."
+			   }
+			}
+		 ],
+		 "suggestions": []
+		  }
+	   }
+	}
+	return {
+		"speech": "Thank you for using MIT Information.  Keep us in mind when you need more of your on campus information.",
+		"displayText": "Thank you for using MIT Information.  Keep us in mind when you need more of your on campus information.",
+		"data": data,
+		"contextOut": {},
+		"source": "webhook"
+	}
+		
+
+
+if __name__ == '__main__':
+	port = int(os.getenv('PORT', 5000))
+
+	print("Starting app on port %d" % port)
+
+	app.run(debug=False, port=port, host='0.0.0.0')
