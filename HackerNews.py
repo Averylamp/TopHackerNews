7cb72d254e7d942a68524be3608d508b2e59a130
Avery Lamp <averylamp@gmail.com>
2017-08-19 01:05:25 -0400

    Test

--- HackerNews.py
+++ HackerNews.py
@@ -49,7 +49,7 @@ def addSuggestions(speech = "", suggestions = [], userResponse = True, items = [
       'suggestions': suggestionsTitles
     },
     'systemIntent': {
-      'intent': 'actions.intent.OPTION',
+      'intent': 'actions.intent.NewsArticle',
       'data': {
         '@type': 'type.googleapis.com/google.actions.v2.OptionValueSpec',
         'listSelect': {
